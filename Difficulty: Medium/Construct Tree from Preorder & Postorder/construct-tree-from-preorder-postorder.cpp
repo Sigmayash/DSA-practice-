e03f45b9e6f4791566cf9945b570361f0ccdb8ad
YashSabale <sabaleyash85@gmail.com>
2025-10-08 15:51:39 +0530

    Update construct-tree-from-preorder-postorder.cpp

--- Difficulty: Medium/Construct Tree from Preorder & Postorder/construct-tree-from-preorder-postorder.cpp
+++ Difficulty: Medium/Construct Tree from Preorder & Postorder/construct-tree-from-preorder-postorder.cpp
@@ -48,44 +48,6 @@ Node* constructTree(vector<int> &pre, vector<int> &post) {
     return constructTreeUtil(pre, post, preIndex, 0, size - 1, size, postMap);
 }
 
-int getHeight(Node* root, int h) {
-    if (root == nullptr) return h - 1;
-    return max(getHeight(root->left, h + 1), getHeight(root->right, h + 1));
-}
 
-
-void levelOrder(Node* root) {
-    queue<pair<Node*, int>> q;
-    q.push({root, 0});
-
-    int lastLevel = 0;
-    int height = getHeight(root, 0);
-
-    while (!q.empty()) {
-        auto top = q.front(); q.pop();
-        Node* node = top.first;
-        int lvl = top.second;
-
-        if (lvl > lastLevel) {
-            cout << "\n";
-            lastLevel = lvl;
-        }
-         
-          
-            if (lvl > height) break;
-            
-    
-        if (node->data != -1) cout << node->data << " ";
-        else cout << "N ";
-
-    
-        if (node->data == -1) continue;
-
-        if (node->left == nullptr) q.push({new Node(-1), lvl + 1});
-        else q.push({node->left, lvl + 1});
-
-        if (node->right == nullptr) q.push({new Node(-1), lvl + 1});
-        else q.push({node->right, lvl + 1});
-    }
 }
 };
